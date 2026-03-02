1ac1cae23ae121fd85f010e2f4acbf1d0baa98c0
Cyrus <shantiravi@Mac.lan>
2026-03-02 20:36:17 +0530

    Add pink background behind all page text for better readability

--- components/page-layout.tsx
+++ components/page-layout.tsx
@@ -6,7 +6,7 @@ export function PageLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative min-h-screen">
       <Navbar />
-      <main>{children}</main>
+      <main className="bg-container-soft rounded-3xl mx-4 lg:mx-8 my-4">{children}</main>
       <Footer />
       <CartSidebar />
     </div>
